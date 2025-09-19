57d4bfa5827d15c3a12e2abc430d0404f06dcaa3
ClydeViscione <191877265+ClydeViscione@users.noreply.github.com>
2025-09-19 13:56:23 +0200

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,25 +1,38 @@
-User_input = input("Select a word: " )
-Number_of_word = len(User_input)
-underscore_space = '_' * Number_of_word
+import random
+from english_words import get_english_words_set
 
-print ("The word :",underscore_space)
-print ()
+Set_of_Word = get_english_words_set(['web2'], lower=True)
+Random_word = random.choice(list(Set_of_Word))
+Number_of_letter = len(Random_word)
 
+revealed_word = ["_"] * Number_of_letter
+Live = Number_of_letter
 
-Live = int(Number_of_word)
-Ask_for_letter = input("Enter: ")
+print ("The word :", " ".join(revealed_word))
+print(Random_word)
 
-while Live != 0:
-	if Ask_for_letter in User_input:
-		print ("The word: ", Ask_for_letter)
-	elif Ask_for_letter not in User_input:
-		Live = Live - 1
-		print ("There's no",Ask_for_letter)
-		print ("Live leftover: ",Live)
-		print ("The word: " )
-	elif Ask_for_letter == User_input:
-		print ("U Win")
-		break
-	else:
-		print ("U Loose")
+while Live > 0:
+    Ask_for_letter = input("Enter a letter or the word : ").lower()
 
+    if Ask_for_letter == Random_word:
+        print ("U win :", Random_word)
+        break
+
+    elif len(Ask_for_letter) == 1 and Ask_for_letter in Random_word:
+        print("the letter", Ask_for_letter, "is in the word.")
+        for i, letter in enumerate(Random_word):
+            if letter == Ask_for_letter:
+                revealed_word[i] = Ask_for_letter
+        print("The word :", " ".join(revealed_word))
+
+        if "_" not in revealed_word:
+            print("U win :", Random_word)
+            break
+
+    else:
+        Live -= 1
+        print("The letter  ", Ask_for_letter, "isn't in the word")
+        print("Live leftover :", Live)
+
+        if Live == 0:
+            print("U loose :", Random_word)
